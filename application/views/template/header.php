2fca304153bd37a1eebe74586f6a3fd877452b3b
Diego Mariano <diogohenks@hotmail.com>
2020-04-03 14:19:08 -0300

    menu

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -17,33 +17,8 @@
 
 				<li class="divider"></li>
 
-				<li class="chapter" data-level="" data-path="index.html">
-					<a href="index.html"><i class="fa fa-check"></i>Prefácio</a>
-				</li>
-				
-				<li class="chapter" data-level="" data-path="acknowledgments.html"><a href="acknowledgments.html"><i class="fa fa-check"></i>Agradecimentos</a></li>
+				<?php $CI->load->view('template/menu',$data); ?>
 
-				<li class="chapter" data-level="1" data-path="getting-started.html"><a href="getting-started.html"><i class="fa fa-check"></i><b>1</b> Introdução</a>
-
-				<li class="part"><span><b>Parte I</b></span></li>
-
-				<li class="chapter" data-level="2" data-path="r-basics.html">
-					<a href="r-basics.html"><i class="fa fa-check"></i><b>2</b> Básico</a>
-
-					<ul style="display: visible;">
-
-						<li class="chapter" data-level="2.1" data-path="r-basics.html"><a href="r-basics.html#case-study-us-gun-murders"><i class="fa fa-check"></i><b>2.1</b> Nível 1</a></li>
-
-							<li class="chapter" data-level="2.2" data-path="r-basics.html"><a href="r-basics.html#the-very-basics"><i class="fa fa-check"></i><b>2.2</b> Outro nível 1</a>
-
-							<ul style="display: visible;">
-						
-								<li class="chapter" data-level="2.2.1" data-path="r-basics.html"><a href="r-basics.html#objects"><i class="fa fa-check"></i><b>2.2.1</b> Nível 2</a></li>
-							</ul>
-						</li>
-					</ul>
-				</li>
-				<li class="chapter" data-level="1" data-path="getting-started.html"><a href="getting-started.html"><i class="fa fa-check"></i><b>6</b> Alinhamento estrutural</a>
 			</ul>
 		</nav>
     </div>
@@ -77,7 +52,7 @@
 		        	<i class="fa fa-circle-o-notch fa-spin"></i><a href="./">Introduction to Data Science</a>
 		        </h1>
         	</div>
-        	
+
 	        <div class="page-wrapper" tabindex="-1" role="main">
 	          <div class="page-inner">
 	            <section class="normal" id="section-">
---
+++ application/views/template/menu.php
@@ -0,0 +1,27 @@
+<li class="chapter" data-level="" data-path="index.html">
+	<a href="index.html"><i class="fa fa-check"></i>Prefácio</a>
+</li>
+
+<li class="chapter" data-level="" data-path="acknowledgments.html"><a href="acknowledgments.html"><i class="fa fa-check"></i>Agradecimentos</a></li>
+
+<li class="chapter" data-level="1" data-path="getting-started.html"><a href="getting-started.html"><i class="fa fa-check"></i><b>1</b> Introdução</a>
+
+<li class="part"><span><b>Parte I</b></span></li>
+
+<li class="chapter" data-level="2" data-path="r-basics.html">
+	<a href="r-basics.html"><i class="fa fa-check"></i><b>2</b> Básico</a>
+
+	<ul style="display: visible;">
+
+		<li class="chapter" data-level="2.1" data-path="r-basics.html"><a href="r-basics.html#case-study-us-gun-murders"><i class="fa fa-check"></i><b>2.1</b> Nível 1</a></li>
+
+			<li class="chapter" data-level="2.2" data-path="r-basics.html"><a href="r-basics.html#the-very-basics"><i class="fa fa-check"></i><b>2.2</b> Outro nível 1</a>
+
+			<ul style="display: visible;">
+		
+				<li class="chapter" data-level="2.2.1" data-path="r-basics.html"><a href="r-basics.html#objects"><i class="fa fa-check"></i><b>2.2.1</b> Nível 2</a></li>
+			</ul>
+		</li>
+	</ul>
+</li>
+<li class="chapter" data-level="1" data-path="getting-started.html"><a href="getting-started.html"><i class="fa fa-check"></i><b>6</b> Alinhamento estrutural</a>
