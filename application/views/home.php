aafb9b50d0d56073a39bdca6ee39847d7ae5d90c
Diego Mariano <diogohenks@hotmail.com>
2020-04-03 15:47:11 -0300

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -1,7 +1,7 @@
 <div id="header">
 	<h1 class="title">Manual prático-teórico de Bioinformática Estrutural</h1>
-	<h2 class="subtitle"><em>Alinhamento estrutural</em></h2>
-	<p class="author"><em>Vitor Pimentel</em></p>
+	<h2 class="subtitle"><em>UFMG</em></h2>
+	<p class="author"><em>Laboratório de Bioinformática e Sistemas</em></p>
 	<p class="date"><em>04 de abril de 2020</em></p>
 	
 	<br>
@@ -14,7 +14,7 @@
 <div class="footnotes">
 	<hr>
 	<ol start="1">
-	<li id="fn115"><p><a href="http://github.com" class="uri">http://github.com</a><a href="introduction-to-productivity-tools.html#fnref115" class="footnote-back">↩</a></p></li>
-	<li id="fn116"><p><a href="https://www.rstudio.com/" class="uri">https://www.rstudio.com/</a><a href="introduction-to-productivity-tools.html#fnref116" class="footnote-back">↩</a></p></li>
+		<li id="fn115"><p><a href="http://bioinfo.dcc.ufmg.br/wiki//home/agradecimentos" class="uri">Agradecimentos</a><a href="introduction-to-productivity-tools.html#fnref115" class="footnote-back">↩</a></p></li>
+		<li id="fn116"><p><a href="http://bioinfo.dcc.ufmg.br/wiki//home/alinhamento_estrutural" class="uri">Alinhamento estrutural</a><a href="introduction-to-productivity-tools.html#fnref116" class="footnote-back">↩</a></p></li>
 	</ol>
 </div>
